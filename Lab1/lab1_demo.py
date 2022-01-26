c1fd9759ec1756750aa62f3bf5898b4df5854c0c
Songhan Yu <songhan@macbook-pro.myfiosgateway.com>
2022-01-26 15:16:52 -0500

    lecture 1

--- Lab1/lab1_demo.py
+++ Lab1/lab1_demo.py
@@ -10,9 +10,12 @@ from functools import reduce
 def inverse(x):
     return 1.0/x
 
+def add(x, y):
+    return x + y
+
 def e(n):
     """ e = 1/0! + 1/1! + 1/2! + 1/3! + ... + 1/n! """
-    return sum(map(inverse, map(factorial, list(range(n+1)))))
+    return reduce(add, map(inverse, map(factorial, list(range(n+1)))))
     
 
 
