a8136c15d42bb563f7c3028be86532de5e494e08
songhany <ysh961008@yeah.net>
2022-01-28 14:46:15 -0500

    homework1

---
+++ assignments/hw1/hw1_SonghanYu.py
@@ -0,0 +1,34 @@
+""" CS515 Homework1
+Name: Songhan Yu
+CWID: 10470449
+"""
+
+from functools import reduce
+
+
+def mult(x, y):
+  """ Returns the product of x and y """
+  return x * y
+
+def factorial(n):
+  """ Input: takes a positive integer n
+      Output: returns n! """
+  return reduce(mult, list(range(1, n+1)))
+
+def add(x, y):
+  """ Returns the sum of x and y """
+  return x + y
+
+def mean(L):
+  """ Input: takes a list
+      Output: returns the mean (average) value in that list """
+  return reduce(add, L) / len(L)
+
+
+# test case
+print(factorial(3))
+print(factorial(4))
+print(factorial(5))
+print(mean([1, 3, 5]))
+print(mean([1, 1, 1]))
+print(mean([1, 3, 5, 7, 9]))
