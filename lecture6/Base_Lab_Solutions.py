33c6d58604cd5586d61903b9c94033954e949b28
songhany <ysh961008@yeah.net>
2022-03-27 23:01:47 -0400

    lecture6

---
+++ lecture6/Base_Lab_Solutions.py
@@ -0,0 +1,74 @@
+
+def isOdd(n):
+    '''Returns whether or not the integer argument is odd.'''
+    return n%2 == 1
+
+def numToBinary(n):
+    '''Precondition: integer argument is non-negative.
+    Returns the string with the binary representation of non-negative integer n.
+    If n is 0, the empty string is returned.'''
+    if n == 0:
+        return ''
+    elif isOdd(n):
+        return numToBinary(n//2) + "1"
+    else:
+        return numToBinary(n//2) + "0"
+
+def binaryToNum(s):
+    '''Precondition: s is a string of 0s and 1s.
+    Returns the integer corresponding to the binary representation in s.
+    Note: the empty string represents 0.'''
+    if s == "":
+        return 0
+    else:
+        if s[0] == '1':
+            return binaryToNum(s[1:]) + 2**(len(s)-1)
+        else:
+            return binaryToNum(s[1:])
+        
+def binaryToNum2(s):
+    if s == "":
+        return 0
+    else:
+        if s[-1] == '1':
+            return binaryToNum(s[:-1])*2 + 1
+        else:
+            return binaryToNum(s[:-1])*2
+def increment(s):
+    '''Precondition: s is a string of 8 bits.
+    Returns the binary representation of binaryToNum(s) + 1.'''
+    dec = binaryToNum(s)
+    b = numToBinary(dec + 1)
+    return '0'* max(0, 8 - len(b)) + b[-8:]
+
+def count(s, n):
+    '''Precondition: s is an 8-bit string and n >= 0.
+    Prints s and its n successors.'''
+    print(s)
+    if n == 0:
+        return
+    else:
+        x = increment(s)
+        return count(x, n-1)
+
+def numToTernary(n):
+    '''Precondition: integer argument is non-negative.
+    Returns the string with the ternary representation of non-negative integer
+    n. If n is 0, the empty string is returned.'''
+    if n == 0:
+        return ''
+    elif n%3 == 1:
+        return numToTernary(n//3) + "1"
+    elif n%3 == 2:
+        return numToTernary(n//3) + "2"
+    elif n%3 == 0:
+        return numToTernary(n//3) + "0"
+
+def ternaryToNum(s):
+    '''Precondition: s is a string of 0s, 1s, and 2s.
+    Returns the integer corresponding to the ternary representation in s.
+    Note: the empty string represents 0.'''
+    if s == "":
+        return 0
+    else:
+        return ternaryToNum(s[:-1])*3 + int(s[-1])
